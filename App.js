ea220c6637d7ab184989ed1ede1423fa2e105296
SnzDev <tms.working@gmail.com>
2020-07-21 20:18:14 -0300

    finished

--- App.js
+++ App.js
@@ -1,224 +1,224 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Alert, TextInput, Vibration, } from 'react-native';
-import { useCallback, useState, Component, } from 'react';
-import { hide } from 'expo/build/launch/SplashScreen';
+import { Text, View, TouchableHighlight, TextInput, Vibration, } from 'react-native';
+import { useState, useEffect } from 'react';
+
+
+import styles from './styles.js'
+
+
 
 export default function App() {
+
+  const [display, setDisplay] = useState('')
+  const [preview, setPreview] = useState('')
+  const [operation, setOperation] = useState('')
+
+  useEffect(() => {
+
+  }, [display, preview, operation]);
+
+  function handleMultiplication() {
+    const lastPreview = preview,lastDisplay = display;
+    setPreview(preview == '' ? display : parseInt(preview) * parseInt(display))
+    setDisplay(lastPreview)
+
+    console.log(operation)
+    console.log('Display: ' + display);
+    console.log('Preview: ' + preview);
+  }
+  function handleSubtraction() {
+
+    setPreview(preview == '' ? display : parseInt(preview) - parseInt(display))
+
+
+    console.log(operation)
+    console.log('Display: ' + display);
+    console.log('Preview: ' + preview);
+  }
+  function handleDivision() {
+
+    setPreview(preview == '' ? display : parseInt(preview) / parseInt(display))
+
+
+    console.log(operation)
+    console.log('Display: ' + display);
+    console.log('Preview: ' + preview);
+  }
+  function handleSum() {
+
+    setPreview(preview == '' ? display : parseInt(preview) + parseInt(display))
+
+
+    console.log(operation)
+    console.log('Display: ' + display);
+    console.log('Preview: ' + preview);
+  }
+
+
   return (
     <View style={styles.container}>
 
       <View>
-      <Text style={styles.title}>CALCULADORA</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.title}>CALCULATOR</Text>
+        <StatusBar style="auto" />
 
       </View>
 
       <View style={styles.display}>
-        <TextInput style={styles.displayfont} placeholder='0' placeholderTextColor="#000" />
-      </View>
+        <TextInput
+          className='first-input'
+          value={display}
+          onChange={e => setDisplay(e.target.value)}
+          style={styles.displayFont}
+          placeholder='0'
+          placeholderTextColor="#000"
+          keyboardType={'numeric'} />
 
-      <View style= {styles.bloco}>
-        
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>CE</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>%</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>/</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>X</Text>
-        </View>
-      </TouchableHighlight>
-      </View>
-
-      <View style= {styles.bloco}>
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>7</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>8</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>9</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>-</Text>
-        </View>
-      </TouchableHighlight>
-      </View>
-
-
-      <View style= {styles.bloco}>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>4</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>5</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>6</Text>
-        </View>
-      </TouchableHighlight>
-
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>+</Text>
-        </View>
-      </TouchableHighlight>
+        <Text style={{ width: 275, marginTop: -83, textAlign: 'right' }}>
+          {preview}
+        </Text>
+        <Text style={{ marginTop: -20, marginLeft: 10 }}>
+          {operation}
+        </Text>
 
       </View>
 
-      <View style= {styles.bloco}>
+      <View style={styles.packs}>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>1</Text>
-        </View>
-      </TouchableHighlight>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>CE</Text>
+          </View>
+        </TouchableHighlight>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>2</Text>
-        </View>
-      </TouchableHighlight>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>%</Text>
+          </View>
+        </TouchableHighlight>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>3</Text>
-        </View>
-      </TouchableHighlight> 
-      
-      <TouchableHighlight>
-        <View style={styles.buttonh} >
-          <Text style={styles.fontsize}> </Text>
-        </View>
-      </TouchableHighlight> 
+        <TouchableHighlight onPress={() => { setOperation('/'), handleDivision() }}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>/</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={() => { setOperation('*'), handleMultiplication()}}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>X</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+
+      <View style={styles.packs}>
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>7</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>8</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>9</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={() => { setOperation('-'), handleSubtraction() }}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>-</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+
+
+      <View style={styles.packs}>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>4</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>5</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>6</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={() => { setOperation('+'), handleSum() }}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>+</Text>
+          </View>
+        </TouchableHighlight>
 
       </View>
-      <View style= {styles.bloco}>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button0} >
-          <Text style={styles.fontsize}>0</Text>
-        </View>
-      </TouchableHighlight>
+      <View style={styles.packs}>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button} >
-          <Text style={styles.fontsize}>,</Text>
-        </View>
-      </TouchableHighlight> 
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>1</Text>
+          </View>
+        </TouchableHighlight>
 
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.buttonEquals} >
-          <Text style={styles.fontsize}>=</Text>
-        </View>
-      </TouchableHighlight> 
-      
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>2</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>3</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight>
+          <View style={styles.buttonh} >
+            <Text style={styles.fontsize}> </Text>
+          </View>
+        </TouchableHighlight>
+
+      </View>
+      <View style={styles.packs}>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button0} >
+            <Text style={styles.fontsize}>0</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.button} >
+            <Text style={styles.fontsize}>,</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={onPress}>
+          <View style={styles.buttonEquals} >
+            <Text style={styles.fontsize}>=</Text>
+          </View>
+        </TouchableHighlight>
+
       </View>
 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor:'grey',
-    width: 68,
-    height: 87,
-    borderRadius: 5,
-    margin: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 2,
-    borderColor:'black',
-  },
-  bloco: {
-    flexDirection:'row',
-  },
-  button0: {
-    backgroundColor: 'grey',
-    paddingVertical:28.05,
-    paddingHorizontal:62.5,
-    borderRadius:5,
-    margin:2,
-    borderWidth:2,
-    borderColor:'black',
-  },
-  fontsize: {
-    fontSize: 20,
-  },
-  buttonEquals: {
-    backgroundColor: 'grey',
-    paddingVertical:73.5,
-    paddingHorizontal:27.2,
-    borderRadius:5,
-    margin:2,
-    marginTop: '-121.5%',
-    borderWidth: 2,
-    borderColor: 'black',
-  },
-  display: {
-    backgroundColor: '#ccc',
-    width: 285,
-    height:91,
-    borderRadius: 5,
-    marginBottom: 10,
-    borderWidth: 3,
-    borderColor: 'black',
-  },
-  displayfont:{
-    fontSize:64,
-    color:'black',
-    textAlign: 'right',
-  },
-  title:{
-    fontSize: 32,
-  },
-  buttonh: {
-    width: 68,
-    height: 87,
-    margin: 2,
-  },
-});
 
-const onPress=() => Vibration.vibrate(50)
+
+const onPress = () => Vibration.vibrate(50)
